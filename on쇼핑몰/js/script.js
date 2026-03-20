b0856542ec36846ff74775bbd4c0f01ab2f8aa3f
seongeun040624 <seongeun040624@gmail.com>
2026-03-20 17:01:03 +0900

    26

---
+++ on쇼핑몰/js/script.js
@@ -0,0 +1,48 @@
+$(function(){
+    //nav
+    $('#nav>ul>li').hover(function(){
+        $(this).find('ul').stop().slideDown();
+    }, function(){
+        $(this).find('ul').stop().slideUp();
+    });
+
+    //slide
+    let current = 0;
+    let slide = $('.slider ul li');
+
+    setInterval(function(){
+        let prev = slide.eq(current);
+        move(prev, 1, 0);
+
+        current++;
+
+        if(current == slide.length){current=0;}
+
+        let next = slide.eq(current);
+        move(next, 0, 1);
+    },3000);
+
+    function move(tg, start, end){
+        tg.css('opacity', start).stop().animate({opacity:end}, 500)
+    }
+
+    //tab
+    $('.tab ul li').click(function(e){
+        e.preventDefault();
+        $('.tab ul li').removeClass('active');
+        $(this).addClass('active');
+
+        let i = $(this).index();
+
+        $('.tabContents>div').hide();
+        $('.tabContents>div').eq(i).show();
+    });
+
+    //popup
+    $('.click').click(function(){
+        $('.popup').show();
+    });
+    $('.close').click(function(){
+        $('.popup').hide();
+    });
+})
